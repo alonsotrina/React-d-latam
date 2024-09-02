cc1b01e714c087483972937750a36a598b7abd68
alonso trina <alonsotrina22@gmail.com>
2024-09-01 21:51:45 -0400

    actualización del componente navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,14 @@
-import { Badge } from "../ui/Badge";
+import { Link } from "react-router-dom";
+import { Badge } from "../ui/Badge"
+import { ShoppingCart } from 'lucide-react';
 
-const NavbarComponets = ({totalItem, total}) => {
-  const token = false;
-  // let formatter = new Intl.NumberFormat("es-cl", {
-  //   minimumFractionDigits: 0,
-  //   maximumFractionDigits: 0,
-  // });
 
+const NavbarComponets = ({ totalItem }) => {
   return (
-    <nav className="flex items-center justify-between flex-wrap p-6 absolute top-0 left-0 right-0 z-10">
-      <h2 className="font-semibold text-xl tracking-tight text-white mr-6">
-        Pizzería Mamma Mia
-      </h2>
+    <nav className="flex items-center justify-between flex-wrap p-6 absolute top-0 left-0 right-0 z-10 bg-dark-900/20">
+      <Link to="/">
+        <h2 className="font-semibold text-xl tracking-tight text-white mr-6">Pizzería Mamma Mia</h2>
+      </Link>
 
       <div className="block lg:hidden">
         <button
@@ -34,54 +31,16 @@ const NavbarComponets = ({totalItem, total}) => {
         className="w-full block flex-grow lg:flex lg:items-center lg:w-auto"
       >
         <div className="text-sm lg:flex-grow">
-          <a
-            href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-md text-white hover:text-white font-semibold mr-4"
-          >
-            🍕 Home
-          </a>
-
-          {/* Variable si existe el token */}
-          {token ? (
-            <>
-              <a
-                href="#responsive-header"
-                className="block mt-4 lg:inline-block lg:mt-0 text-md text-white hover:text-white font-thin mr-4"
-              >
-                🔓 Profile
-              </a>
-              <a
-                href="#responsive-header"
-                className="block mt-4 lg:inline-block lg:mt-0 text-md text-white hover:text-white font-semibold"
-              >
-                🔓 Logout
-              </a>
-            </>
-          ) : (
-            <>
-              <a
-                href="#responsive-header"
-                className="block mt-4 lg:inline-block lg:mt-0 text-md text-white hover:text-white font-semibold mr-4"
-              >
-                🔐 Login
-              </a>
-              <a
-                href="#responsive-header"
-                className="block mt-4 lg:inline-block lg:mt-0 text-md text-white hover:text-white font-semibold"
-              >
-                🔐 Register
-              </a>
-            </>
-          )}
+          <Link to="login" className="block mt-4 lg:inline-block lg:mt-0 text-md text-white hover:text-white font-semibold mr-3">login</Link>
+          <Link to="register" className="block mt-4 lg:inline-block lg:mt-0 text-md text-white hover:text-white font-semibold mr-3">Register</Link>
+          <Link to="profile" className="block mt-4 lg:inline-block lg:mt-0 text-md text-white hover:text-white font-semibold mr-3">Profile</Link>
         </div>
         <div>
-         
-          <a
-            href="#"
-            className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-dark-700 hover:bg-white mt-4 lg:mt-0"
-          >
-            {total} {totalItem > 0 ? <Badge variant="danger" size="default">{totalItem}</Badge> : null}
-          </a>
+
+          <Link to="cart" className="inline-flex text-sm px-2 py-2 leading-none rounded text-white bg-warning hover:border-transparent hover:text-dark-700 hover:bg-white mt-4 lg:mt-0" >
+            <ShoppingCart size={16} color="#000" />
+            {totalItem > 0 ? <Badge variant="danger" size="default" className="mx-2">{totalItem}</Badge> : null}
+          </Link>
         </div>
       </div>
     </nav>

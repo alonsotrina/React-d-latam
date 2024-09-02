057b5f0c845eb4b64480f020ac28265a14904032
alonso trina <alonsotrina22@gmail.com>
2024-09-01 21:48:12 -0400

    creación de la page Profile.jsx

---
+++ src/Home/Profile.jsx
@@ -0,0 +1,23 @@
+import { Button } from '../components/ui/Buttton';
+import background from '../assets/img/header/image.png'
+
+const Profile = () => {
+    return (
+        <div
+            className="flex justify-center items-center"
+            style={{
+                height: 'calc(100vh - 121px)',
+                backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.458)), url(${background})`,
+                backgroundPosition: 'center', // Centra la imagen
+            }}
+
+        >
+            <div className="text-center">
+                <h2 className="text-[3rem] italic font-semibold text-white">alonsotrina2@gmail.com</h2>
+                <Button variant="danger" size="lg">cerrar sesión </Button>
+            </div>
+        </div>
+    )
+}
+
+export default Profile

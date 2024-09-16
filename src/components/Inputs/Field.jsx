dddadf2b2b2480db4e211eaa66796a739142c641
alonso trina <alonsotrina22@gmail.com>
2024-09-16 11:20:54 -0300

    actulización de components base

--- src/components/Inputs/Field.jsx
+++ src/components/Inputs/Field.jsx
@@ -3,7 +3,7 @@ import { EyeIcon } from '@heroicons/react/24/outline';
 const Field = ({ label, name, type, value, onChange, handleBlur, validations }) => {
     return (
         <div>
-            <label className={`block text-[12px] font-semibold leading-6 text-gray-500 capitalize after:content-['*'] after:ml-0.5 after:text-red-500 after:text-lg`}>
+            <label className={`block text-[12px] font-semibold leading-6 text-gray-500 capitalize after:content-['*'] after:ml-0.5 after:text-danger after:text-lg`}>
                 {label}
             </label>
             <div className="mt-1 relative">
@@ -13,14 +13,14 @@ const Field = ({ label, name, type, value, onChange, handleBlur, validations }) 
                     value={value}
                     onChange={onChange}
                     onBlur={handleBlur}
-                    className={`block w-full rounded-md border-0 px-3.5 py-2 text-gray-700 shadow-sm ring-1 ring-inset ${validations ? "ring-red-600": 'ring-gray-300'} placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:outline-none sm:text-sm sm:leading-6 focus:ring-gray-500`}
+                    className={`block w-full rounded-md border-0 px-3.5 py-2 text-gray-700 shadow-sm ring-1 ring-inset ${validations ? "ring-danger": 'ring-dark-300'} placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:outline-none sm:text-sm sm:leading-6 focus:ring-gray-500`}
                 />
                 {type === 'password' ?
                     <EyeIcon className="h-5 w-5 text-gray-400 absolute right-3 top-1/2 transform -translate-y-1/2" />
                     : null
                 }
             </div>
-            {validations && <div className="text-[12px] text-red-600 font-semibold mt-1">{validations}</div>}
+            {validations && <div className="text-[12px] text-danger font-semibold mt-1">{validations}</div>}
         </div>
     )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,28 @@
 import { useCart } from "../../context/CartContext"
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { Badge } from '../index';
 import { ShoppingCart } from 'lucide-react'
 import { formatter } from '../../utils/formatters';
+import { useAuth } from "../../context/AuthContext";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../index"
 
-const NavbarComponets = () => {
+
+const NavbarComponets = ({ className }) => {
   const { total, totalItem } = useCart()
+  const { auth, logout } = useAuth()
+
+  const setActiveClass = ({ isActive }) => (isActive ? "nav-active nav-link" : 'nav-link');
+
   return (
-    <nav className="flex items-center justify-between flex-wrap p-6 absolute top-0 left-0 right-0 z-10 bg-dark-900/20">
+    // <nav className="flex items-center justify-between flex-wrap p-6 absolute top-0 left-0 right-0 z-10 bg-dark-900/20">
+    <nav className={`flex items-center justify-between flex-wrap p-6 ${className}`}>
       <Link to="/" className="font-semibold text-xl tracking-tight text-white mr-6">
         Pizzería Mamma Mia
       </Link>
@@ -33,16 +48,29 @@ const NavbarComponets = () => {
         className="w-full block flex-grow lg:flex lg:items-center lg:w-auto"
       >
         <div className="text-sm lg:flex-grow">
-          <Link to="login" className="block mt-4 lg:inline-block lg:mt-0 text-md text-white hover:text-white font-semibold mr-3">login</Link>
-          <Link to="register" className="block mt-4 lg:inline-block lg:mt-0 text-md text-white hover:text-white font-semibold mr-3">Register</Link>
-          <Link to="profile" className="block mt-4 lg:inline-block lg:mt-0 text-md text-white hover:text-white font-semibold mr-3">Profile</Link>
+          {auth.token ? null : <NavLink to="login" className={setActiveClass}>login</NavLink>}
+          {auth.token ? null : <NavLink to="register" className={setActiveClass}>Register</NavLink>}
+          {auth.token ? <NavLink to="profile" className={setActiveClass}>Profile</NavLink> : null}
+          {auth.token ? <Link to="/" className="nav-link" onClick={logout}>Cerrar sesión</Link> : null}
         </div>
 
-        <div>
+        <div className="flex items-center">
+
+          {/* <DropdownMenu>
+            <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+            <DropdownMenuContent className="bg-danger text-white border-none min-w-[200px] mr-8 px-0">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator className="border border-primary"  />
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Billing</DropdownMenuItem>
+              <DropdownMenuItem>Team</DropdownMenuItem>
+              <DropdownMenuItem>Subscription</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu> */}
           <Link to="cart" className="inline-flex text-sm px-2 py-2 leading-none rounded text-white bg-warning hover:border-transparent hover:text-dark-700 hover:bg-white mt-4 lg:mt-0" >
             <ShoppingCart size={16} color="#000" />
             {totalItem > 0 ? <Badge variant="danger" size="default" className="mx-2">{totalItem}</Badge> : null}
-            {formatter.format(total)}
+            {/* {formatter.format(total)} */}
           </Link>
         </div>
       </div>

15a6e165e1fd1085f97c1968e6d6d7e945a0b8f7
alonso trina <alonsotrina22@gmail.com>
2024-09-16 11:22:06 -0300

    creación de rutas protegidas

--- src/router/MainRoutes.jsx
+++ src/router/MainRoutes.jsx
@@ -1,16 +1,30 @@
-import { Routes, Route} from "react-router-dom";
-import { Home, PizzaDetail, LoginPage, RegisterPage, NotFound, Profile, Cart } from '../views'
+import { Routes, Route, Navigate } from "react-router-dom";
+import { HomePage, PizzaDetail, LoginPage, RegisterPage, NotFound, ProfilePage, CartPage } from '../views'
+import { useAuth } from "../context/AuthContext";
+import { HomeLayout } from "../layout/HomeLayout";
+import { InternalLayout } from "../layout/InternalLayout";
 
 
 const MainRoutes = () => {
+    const { auth } = useAuth()
+    console.log('auth', auth.token)
     return (
         <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<LoginPage />} />
-            <Route path='/register' element={<RegisterPage />} />
-            <Route path='/profile' element={<Profile />} />
-            <Route path='/cart' element={<Cart />} />
-            <Route path='/pizza/p001' element={<PizzaDetail />} />
+
+            {/* Layout para el Home */}
+            <Route path="/" element={<HomeLayout />}>
+                <Route index element={<HomePage />} />
+            </Route>
+
+            {/* Layout para las rutas internas (login, register, profile, pizza detail) */}
+            <Route element={<InternalLayout />}>
+                <Route path="login" element={auth.token ? <Navigate to="/" /> : <LoginPage />} />
+                <Route path="register" element={auth.token ? <Navigate to="/" /> : <RegisterPage />} />
+                <Route path='cart' element={ <CartPage />} />
+                <Route path='profile' element={auth.token ? <ProfilePage /> : <Navigate to="/" />} />
+                <Route path="pizza-detail/:id" element={<PizzaDetail />} />
+            </Route>
+
             <Route path='/*' element={<NotFound />} />
         </Routes>
     )
